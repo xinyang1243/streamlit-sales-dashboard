e6d20fb07697e708ee5c1fe9cf94580298ccea32
xinyang1243 <117121115+xinyang1243@users.noreply.github.com>
2022-11-10 02:23:49 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,18 +1,10 @@
-# @Email:  contact@pythonandvba.com
-# @Website:  https://pythonandvba.com
-# @YouTube:  https://youtube.com/c/CodingIsFun
-# @Project:  Sales Dashboard w/ Streamlit
-
-
-
 import pandas as pd  # pip install pandas openpyxl
 import plotly.express as px  # pip install plotly-express
 import streamlit as st  # pip install streamlit
 
-# emojis: https://www.webfx.com/tools/emoji-cheat-sheet/
 st.set_page_config(page_title="Sales Dashboard", page_icon=":bar_chart:", layout="wide")
 
-# ---- READ EXCEL ----
+# ---- READ EXCEL FILE ----
 @st.cache
 def get_data_from_excel():
     df = pd.read_excel(
